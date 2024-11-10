b1a287538fe4b72b9147d2a2e0c775db2812186e
David Daly <ddalyjr@gmail.com>
2024-11-10 15:09:57 -0500

    add drizzle db to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image
  from "next/image";
 import { mock } from "node:test"
+import { db } from "~/server/db"
 const mockUrls = [
   "https://utfs.io/f/E0EtgfoZ0Ho5gvZSAJeN02mWs6eTX5hEAHy7pMqt3ox8ijSB",
   "https://utfs.io/f/E0EtgfoZ0Ho59nUoH3QOQ8hJelnYHUF2ILzrqZicysC34t7f",
@@ -14,11 +15,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts)
+
   return (
     <main>
-      <div className="flex flex-wrap gap-4 p-2"> {
-        [...mockImages,...mockImages,...mockImages].map((image, idx) => (
+      <div className="flex flex-wrap gap-4 p-2">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages,...mockImages,...mockImages].map((image, idx) => (
           <div key={idx} className="w-48">
             <img src={image.url} />
           </div>
