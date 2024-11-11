1a3b48566397749f7d4ad0a46e03ee80c314d552
David Daly <ddalyjr@gmail.com>
2024-11-11 01:49:34 -0500

    update database table name and readme

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## TODO
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
-- [ ] Tidy up build process
-- [ ] Actually set up a database
-- [ ] Attach database to UI
+- [x] Tidy up build process
+- [x] Actually set up a database
+- [ ] Attach database to UI and build schema
 - [ ] Add authentication (w/ clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,28 @@
 import Link from "next/link";
 import Image
  from "next/image";
-import { mock } from "node:test"
 import { db } from "~/server/db"
 
 export const dynamic = "force-dynamic"
 
-const mockUrls = [
-  "https://utfs.io/f/E0EtgfoZ0Ho5gvZSAJeN02mWs6eTX5hEAHy7pMqt3ox8ijSB",
-  "https://utfs.io/f/E0EtgfoZ0Ho59nUoH3QOQ8hJelnYHUF2ILzrqZicysC34t7f",
-  "https://utfs.io/f/E0EtgfoZ0Ho5pTUaUh01SesgCKcVFPmWxvHulAdanZYytwU9",
-  "https://utfs.io/f/E0EtgfoZ0Ho5SJRQuTImSZg2kXqrlT3LPhn5swJHOKEjD1pA"
-]
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts)
+  const images = await db.query.images.findMany();
+  console.log(images)
 
   return (
     <main>
       <div className="flex flex-wrap gap-4 p-2">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages,...mockImages,...mockImages].map((image, idx) => (
-          <div key={idx} className="w-48">
+        {images.map((image) => (
+          <div key={image.id} className="w-48 h-32">
             <img src={image.url} />
           </div>
-        ))
-      }
+        ))}
       </div>
-      Hello (gallery in progress)
-      <a href="/layer2">forward</a>
+      <div className="text-5xl font-extrabold">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+        Hello World
+        </span>
+      </div>
     </main>
   );
 }
