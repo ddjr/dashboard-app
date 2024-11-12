0cda2eec00f56aaa0de54e02180ad06069da19e8
David Daly <ddalyjr@gmail.com>
2024-11-12 08:06:51 -0500

    add image to modal

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default async function PhotoModal(props: {
+// export default async function PhotoModal(props: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const params = await props.params;
+//   const { id: photoId } = params;
+//   return <div>{photoId}</div>;
+// }
+
+import { getImage } from "~/server/queries";
+
+export default async function Page({
+  params,
+}: {
   params: Promise<{ id: string }>;
 }) {
-  const params = await props.params;
-  const { id: photoId } = params;
-  return <div>{photoId}</div>;
+  const photoId = Number((await params).id);
+  const { url } = await getImage(photoId);
+  return (
+    <div>
+      <img src={url} className="w-96" />
+    </div>
+  );
 }
