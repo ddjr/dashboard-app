2c795802f6d0c3e367c2c5be39a07238a20759df
David Daly <ddalyjr@gmail.com>
2024-11-12 08:57:10 -0500

    update modal and full page spacing

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,6 +1,17 @@
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
-  const { url } = await getImage(props.id);
-  return <img src={url} className="w-96" />;
+  const image = await getImage(props.id);
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className="flex w-52 flex-shrink-0 flex-col border-l border-white">
+        <div className="overflow-hidden text-ellipsis text-xl font-bold">
+          {image.name}
+        </div>
+      </div>
+    </div>
+  );
 }
