073ad81ab7e3d326ddf655a0302ce4002c81e3cb
David Daly <ddalyjr@gmail.com>
2024-11-12 10:12:12 -0500

    finalize modal and parallel image routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,15 +1,26 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
-        <img src={image.url} className="flex-shrink object-contain" />
+      <div className="m-auto flex items-center justify-center">
+        <img src={image.url} className="object-contain" />
       </div>
-      <div className="flex w-52 flex-shrink-0 flex-col border-l border-white">
-        <div className="overflow-hidden text-ellipsis text-xl font-bold">
+      <div className="flex min-w-52 flex-col border-l border-white backdrop-blur-sm">
+        <div className="overflow-hidden text-ellipsis border-b p-2 text-center text-xl">
           {image.name}
+        </div>
+        <div className="flex flex-col p-2">
+          <span>uploaded by: </span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created on: </span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
         </div>
       </div>
     </div>
